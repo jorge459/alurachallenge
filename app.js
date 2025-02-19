42f0aef5572643bb364267c6245adf06e6353afb
jorge <jorgef2465@gmail.com>
2025-02-18 20:02:28 -0500

    implementado la funcion de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let participantes =[];
+function agregarParticipantes(){
+    let nuevoParticipante = document.getElementById('amigo');
+    let participante = nuevoParticipante.value.trim();
+
+    if(participante == ''){
+        alert ("El campo esta vacio ingrese un nombre valido")
+    }else{
+        participantes.push(participante);
+    }
+    nuevoParticipante.value='';
+    return;
+}
+
+
+
